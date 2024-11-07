7b8384fb482a11e006ace2d1c7391e738b744986
Odility <91459943+odility@users.noreply.github.com>
2024-11-07 05:32:54 +0000

    main.tf update

--- main.tf
+++ main.tf
@@ -9,3 +9,18 @@ module "ec2" {
     instance_type = "t2.micro"
 
 }
+
+resource "aws_s3_bucket" "s3_bucket" {
+  bucket = "odility-s3" 
+}
+
+resource "aws_dynamodb_table" "terraform_lock" {
+  name           = "terraform-lock"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
